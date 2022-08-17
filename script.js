61b8a4c1728e60217d6e6024786cb34ee6747d1a
SUREENASLAWI <sureen130@gmail.com>
2022-08-17 20:01:35 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function BMI (weight, height) {
+   let result = weight / (height * height);
+   return result ;
+}
 
+console.log (BMI(80, 1.7)) ;
+
+function Status(bim) {
+    if ( bim < 18.5){
+        return "لديك نقص في الوزن";
+    } else if ( bim >= 18.5 && bim < 25) {
+        return "وزنك صحي";
+    } else {
+        return "لديك زيادة في الوزن";
+    }
+}
+
+function calculate(){
+    let weight = document.getElementById('weight').value ;
+    let height = document.getElementById('height').value ;
+
+    let bmi = BMI(weight,height);
+
+    let desc = Status (bim);
+
+    let div = document.getElementById("result");
+    
+    div.innerText = "bmi + " == " + desc"
+
+}
